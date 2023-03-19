c1a4db684fa75fc9954b93843b2bc812a18e18ad
SvySor <svysor@gmail.com>
2023-03-19 09:21:27 +0300

    Решена задача 2 и 3

---
+++ Seminar01/HW_03.java
@@ -0,0 +1,39 @@
+package Seminar01;
+
+import java.util.Scanner;
+
+public class HW_03 {
+    public static void main(String[] args) {
+        double number01;
+        double number02;
+        double answer;
+        char operation;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter 1st number: ");
+        number01 = scanner.nextDouble();
+        System.out.print("Enter 1st number: ");
+        number02 = scanner.nextDouble();
+        System.out.print("\nEnter an operator (+, -, *, /): ");
+        operation = scanner.next().charAt(0);
+        switch (operation) {
+            case '+':
+                answer = number01 + number02;
+                break;
+            case '-':
+                answer = number01 - number02;
+                break;
+            case '*':
+                answer = number01 * number02;
+                break;
+            case '/':
+                answer = number01 / number02;
+                break;
+            default:
+                System.out.printf("Error! Enter correct operator");
+                return;
+        }
+        System.out.print("\nThe answer is: ");
+        System.out.printf(number01 + " " + operation + " " + number02 + " = " + answer);
+        scanner.close();
+    }
+}
